3815d6730ef33668b0b82a7ac24e51ce8361624d
KlausC <klausC@users.noreply.github.com>
2025-05-09 15:34:25 +0200

    plot

--- src/evaluatepower.jl
+++ src/evaluatepower.jl
@@ -10,6 +10,7 @@ using CairoMakie
 
 const MIN_PER_HOUR = 60
 const F = Float64
+const PLOT_DIR = joinpath(homedir(), "Dokumente", "Energie", "Buderus", "Plots")
 
 function evaluate_power(time::DateTime, hours::Integer=1)
     fems = FEMS.read_fems_files()
@@ -53,16 +54,38 @@ function evaluate_power(fems, heat::TimedCollection{Energy.HeatData}, time::Date
     fd, hd2, toutd, tflow, thotw
 end
 
+function filterticks(t0, ticks, nvisible)
+    n = maximum(ticks) - minimum(ticks)
+    nticks = length(ticks)
+    ticklabels = fill("", nticks)
+    vismin = tickminutes(Int(floor(n * length(ticks) / nvisible / 2)))
+    i = 0
+    for it = ticks
+        i += 1
+        t = t0 + Minute(it)
+        if minute(t) % vismin == 0
+            label = Dates.format(t, "H:MM")
+            ticklabels[i] = label
+        end
+    end
+    ticklabels
+end
+
+function tickminutes(n)
+    n >= 2000 ? 60 : n > 1000 ? 30 : n > 480 ? 15 : n > 160 ? 5 : 1
+end
+
 function plot_power(time, hours::Integer=1)
     fd, hd, toutd, tflow, thotw = evaluate_power(time, hours)
     n = length(fd)
-    title = string(Dates.format(time, "yyyy-mm-dd HH:MM"), " $(hours*MIN_PER_HOUR) Minutes")
-    xticks = 0:15:n
+    title = string(Dates.format(time, "yyyy-mm-dd H:MM"), " - ", Dates.format(time+Minute(n), "H:MM"))
+    mticks = 0:tickminutes(n):n
     yticks = 0:0.5:7
-    xlabel = "Minutes"
+    xlabel = "HH:MM"
     ylabel = "kW"
     #guidefont = (11, :black)
     taxis = 0:n-1
+    xticks = (mticks, filterticks(time, mticks, 10))
 
     f = Figure(; size=(1900, 600))
     ax = Axis(f[1, 1]; title, xlabel, ylabel, xticks, yticks)
@@ -78,8 +101,7 @@ function plot_power(time, hours::Integer=1)
     #lines!(axt, taxis, blowup(thotw .- 40); label="heißwasser-40")
 
     axislegend(axt, "Temperatur"; position=:rt)
-    display(f)
-    save("plot_$(time)_$(hours).svg", f)
+    save(joinpath(PLOT_DIR, "plot_$(time)_$(hours).svg"), f)
     f
 end
 
